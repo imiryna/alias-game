e896087375d9bd1254a503603f4f2e51cccd76dc
Iryna Burlaka <i.am.blinova@gmail.com>
2025-10-14 11:00:52 +0200

    Feature #3 finish

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", routerApi);
+
+app.use((_, res, _) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status).json({ message: err.message });
+});
+
+module.exports = app;
