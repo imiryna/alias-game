573dce5d8e789bfcce31904e629b0dc14d3fbd6c
Iryna Burlaka <i.am.blinova@gmail.com>
2025-10-16 13:32:07 +0200

    hotfix #14 mongoose schemas chat/team

--- models/teamModel.js
+++ models/teamModel.js
@@ -1,22 +1,38 @@
 const { model, Schema } = require("mongoose");
 
-const teamSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  player_list: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User", // reference to players
+const teamSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-  team_score: {
-    type: Number,
-    default: 0,
+    player_list: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User", // reference to players
+      },
+    ],
+    team_score: {
+      type: Number,
+      default: 0,
+    },
+    // 👇 current explainer must be one of the players
+    current_explainer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      validate: {
+        validator: function (value) {
+          // ensure explainer belongs to player_list
+          return !value || this.player_list.some((id) => id.equals(value));
+        },
+        message: "Current explainer must be one of the team's players",
+      },
+    },
   },
-});
+  { timestamps: true }
+);
 
-const TeamModel = model("game", teamSchema);
+const TeamModel = model("team", teamSchema);
 
 module.exports = TeamModel;
