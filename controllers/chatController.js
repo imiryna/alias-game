3036e8172d21fdcd08ccacd3d37628ec2b5ea943
Kateryna Shvets <32341036+Enigma2017@users.noreply.github.com>
2025-10-19 03:00:00 +0300

    hotfix#17 fixed chat controller and added chat service,

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,13 +1,11 @@
-const ChatModel = require("../models/chatModel");
+const chatService = require("../services/chatService");
 const { catchAsync, HttpError } = require("../utils");
 
-// to get chat by team ID
+// to get a chat by team ID
 exports.getChatByTeam = catchAsync(async (req, res) => {
   const { teamId } = req.params;
 
-  const chat = await ChatModel.findOne({ team_id: teamId })
-    .populate("messages.user", "username email")
-    .populate("explainer", "username email");
+  const chat = await chatService.getChatByTeam(teamId);
 
   if (!chat) {
     throw new HttpError(404, "Chat not found for this team");
@@ -16,19 +14,18 @@ exports.getChatByTeam = catchAsync(async (req, res) => {
   res.status(200).json(chat);
 });
 
-// to create chat for a team
+// to create a chat for a team
 exports.createChatForTeam = catchAsync(async (req, res) => {
   const { teamId } = req.params;
 
-  const existingChat = await ChatModel.findOne({ team_id: teamId });
-  if (existingChat) {
+  const chat = await chatService.createChatForTeam(teamId);
+
+  if (!chat) {
     throw new HttpError(400, "Chat for this team already exists");
   }
 
-  const chat = await ChatModel.create({ team_id: teamId, messages: [] });
-
   res.status(201).json({
-    message: "Chat created",
+    message: "Chat created successfully",
     chat,
   });
 });
